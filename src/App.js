1be6b2e65a7d03f0527347baf6acbef7c2aced67
Saara Leppis <saara@leppihalme.fi>
2022-01-24 12:44:56 +0200

    router, outlet, routes added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 
 import './App.css'
 import Header from './components/Header';
 import PokeList from './components/PokeList';
+import Home from './components/Home';
+import Layout from './components/Layout';
+import Loader from './components/Loader';
 
 const App = () => {
 
   return (
-    <div>
-      <Header />
-      <PokeList />
-
-    </div >
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="pokelist" element={<PokeList />} />
+        </Route>
+      </Routes>
+    </BrowserRouter >
   );
 }
 
